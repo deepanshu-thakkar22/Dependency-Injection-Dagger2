8b204e4ce2082948fc3bbc44c79e7585018a650b
deepanshu.thakkar <deepanshu.thakkar@acsicorp.com>
2023-03-02 17:59:26 +0530

    Dagger2- Use of Modules to understand how to Provide dependencies of Interfaces

--- app/src/main/java/com/example/dependency_injection_dagger2/service/MainActivityPresenter.kt
+++ app/src/main/java/com/example/dependency_injection_dagger2/service/MainActivityPresenter.kt
@@ -4,13 +4,13 @@ import com.example.dependency_injection_dagger2.repository.UserRepository
 import javax.inject.Inject
 
 class MainActivityPresenter @Inject constructor(
-    private val emailService: EmailService,
+    private val notificationService: NotificationService,
     private val userRepository: UserRepository
 ) {
 
     fun registerUser(email:String,password:String){
         userRepository.saveUser(email,password)
-        emailService.send(email,"deepanshu@gmail.com","User Registered")
+        notificationService.send(email,"deepanshu@gmail.com","User Registered")
     }
 
 }
